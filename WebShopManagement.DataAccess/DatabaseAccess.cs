479b7fc80b400afb025f0d5699009068ef53861c
MINH NHUC <MINH NHUC@DESKTOP-VVQQO5P>
2019-12-29 15:54:44 +0700

    Add project files.

---
+++ WebShopManagement.DataAccess/DatabaseAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WebShopManagement.DataAccess
+{
+    public class DatabaseAccess
+    {
+        SqlConnection con;
+        public DatabaseAccess()
+        {
+            var configuration = GetConfiguration();
+            con = new SqlConnection(configuration.GetSection("Data").GetSection("NhucConnectionString").Value);
+        }
+        public IConfigurationRoot GetConfiguration()
+        {
+            var builer = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            return builer.Build();
+        }
+    }
+}
---
+++ WebShopManagement.DataAccess/Model/M_Products.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShopManagement.DataAccess.Model
+{
+    public class M_Products
+    {
+        public long ID { get; set; }
+        public string ProductName { get; set; }
+        public long CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public long UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int Status { get; set; }
+    }
+
+    public enum Products
+    {
+        ID,
+        ProductName,
+        CreatedBy,
+        CreatedDate,
+        UpdatedBy,
+        UpdatedDate,
+        Status
+    }
+    public enum ProductsProcude
+    {
+        P_InsertOrUpdate_M_Products,
+        P_M_Products_GetByID,
+        P_GetAll_M_Products
+    }
+}
